54848be6d4a4036d637ac4acd9d7f7aca9033359
Pratik Bhaskar <pratik.bhaskar@balendin.in>
2022-02-02 13:56:09 +0530

    comment removed on user test case page and add login function

--- src/test/java/in/balendin/schoolmanagment/testcases/user/UserTestCase.java
+++ src/test/java/in/balendin/schoolmanagment/testcases/user/UserTestCase.java
@@ -1,8 +1,32 @@
 package in.balendin.schoolmanagment.testcases.user;
 
-public class UserTestCase {
+import in.balendin.schoolmanagment.steps.LoginSteps;
+import in.balendin.schoolmanagment.testcases.basetest.BaseTestCase;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.WithTagValuesOf;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
-    //  Hello Prathik
+@Narrative(text = {"As a super admin,",
+        "I want to be able to be able to login successfully to the application"})
+@RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@WithTagValuesOf({"login", "regression"})
 
-    //Hello Raju
+
+
+public class UserTestCase extends BaseTestCase {
+    @Steps
+    LoginSteps loginSteps;
+
+    @Before
+    public void loginAsSuperAdmin() {
+        loginSteps.navigateToApplication();
+        loginSteps.loginAsSuperAdmin();
+    }
+
 }
